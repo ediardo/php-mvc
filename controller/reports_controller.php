64b8f91b679b58b447a8b84b0c540e9817bb5172
unknown <jramire4@JRAMIRE4-MOBL1.amr.corp.intel.com>
2013-06-01 15:18:11 -0500

    upload files

---
+++ controller/reports_controller.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of PastesController
+ *
+ * @author ediardoa
+ * a
+ */
+class ReportsController extends Controller{
+    //put your code here
+    
+    /*
+     * Muestra el listado de reportes
+     * @
+     */
+    function index(){
+        
+    }
+    function add(){
+        
+    }
+    
+    function delete(){
+        
+    }
+    
+    function edit(){
+        
+    }
+}
+
+?>
---
+++ model/user.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of comment
+ *
+ * @author ediardo
+ */
+class User extends Model {
+    //put your code here
+    protected $user_id;
+    protected $username;
+    protected $password;
+    protected $email;
+    protected $status;
+    protected $created;
+    protected $modified;
+    
+
+    function __construct(){
+        parent::__construct('User','users');
+    }
+    
+    function test(){
+        echo "test";
+    }
+    function auth($username, $password){
+        $this->execute("SELECT * FROM users WHERE username = '$username' AND password = '$password' LIMIT 1");
+        return $this->result;
+    }
+
+}
+
+?>
